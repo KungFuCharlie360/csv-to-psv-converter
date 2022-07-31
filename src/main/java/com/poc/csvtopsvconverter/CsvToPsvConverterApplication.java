b2a0b1d147a066a445cf8840da59c94c2ba7e143
bradcortez23 <brad.cortez23@gmail.com>
2022-07-31 19:12:34 +0800

    spring boot commit

---
+++ src/main/java/com/poc/csvtopsvconverter/CsvToPsvConverterApplication.java
@@ -0,0 +1,13 @@
+package com.poc.csvtopsvconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvToPsvConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvToPsvConverterApplication.class, args);
+	}
+
+}
